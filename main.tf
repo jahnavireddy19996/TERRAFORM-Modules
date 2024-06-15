8f7e06fdc75715941af4eb4c19ceaddf551eedd6
jahnavireddy19996 <168060923+jahnavireddy19996@users.noreply.github.com>
2024-06-15 19:10:37 +0530

    adding 2 files

---
+++ main.tf
@@ -0,0 +1,58 @@
+resource "aws_vpc" "my-vpc" {
+    cidr_block = var.cidr_block
+    tags = {
+      Name = var.vpc-name
+    }  
+}
+
+resource "aws_internet_gateway" "internet-gateway" {
+    vpc_id = aws_vpc.my-vpc.id
+    tags = {
+    Name = var.igw-name
+}
+}
+resource "aws_subnet" "public-subnet" {
+    vpc_id = aws_vpc.my-vpc.id
+    cidr_block = var.cidr_public
+    availability_zone = var.public-subnet-zone
+    tags = {
+      name = var.public-subnet
+    }
+  
+}
+resource "aws_subnet" "private-subnet" {
+    vpc_id = aws_vpc.my-vpc.id
+    cidr_block = var.cidr_private
+    availability_zone = var.private-subnet-zone
+    tags = {
+      Name = var.private-subnet
+    }
+  
+}
+resource "aws_route_table" "rt-public" {
+    vpc_id = aws_vpc.my-vpc.id
+tags = {
+      Name = var.rt-public
+    }
+}
+resource "aws_route_table" "rt-private" {
+    vpc_id = aws_vpc.my-vpc.id
+    tags = {
+      Name = var.rt-private
+}
+}
+resource "aws_route" "rt" {
+    route_table_id = aws_route_table.rt-public.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+
+}
+resource "aws_route_table_association" "public A" {
+      route_table_id = aws_route_table.rt-public.id
+      subnet_id = aws_subnet.public-subnet.id 
+}
+
+resource "aws_route_table_association" "private A" {
+      route_table_id = aws_route_table.rt-private.id
+      subnet_id = aws_subnet.private-subnet.id 
+}
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "cidr_block" {
+    type = string  
+}
+
+variable "vpc-name" {
+    type = string
+  
+}
+variable "igw-name" {
+    type = string
+  
+}
+variable "cidr_public" {
+    type = string
+  
+}
+variable "public-subnet-zone" {
+    type = string
+}
+
+variable "private-subnet-zone" {
+    type = string
+}
+
+  variable "public-subnet" {
+    type = string
+    
+  }
+  variable "cidr_private" {
+    type = string
+  
+}
+
+variable "private-subnet" {
+    type = string
+  }
+variable "rt-public" {
+    type = string
+    
+  }
+  variable "rt-private" {
+    type = string
+  }
+    
